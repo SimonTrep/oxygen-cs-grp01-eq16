811c30d816f029aa07d2663f3632b82f57796748
SimonTrep <58221264+SimonTrep@users.noreply.github.com>
2023-10-27 14:20:39 -0400

    Test Update + Lint

--- src/dbConnection.py
+++ src/dbConnection.py
@@ -1,20 +1,30 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 import pyodbc
 
 load_dotenv()
 
-#Connexion à la base de données
-def get_conn():
 
+# Connexion à la base de données
+def get_conn():
     database_server_name = os.getenv("DATABASE_SERVER_NAME")
     database_name = os.getenv("DATABASE_NAME")
     user_name = os.getenv("USER_NAME")
     user_password = os.getenv("USER_PASSWORD")
-    connection_string = '''Driver={ODBC Driver 18 for SQL Server};
-                        Server=tcp:''' + str(database_server_name) + '''.database.windows.net,1433;
-                        Database=''' + str(database_name) + ''';
-                        UID=''' + str(user_name) + ''';
-                        PWD=''' + str(user_password) + ''';
-                        Encrypt=yes;TrustServerCertificate=no;Connection Timeout=-1'''
+    connection_string = (
+        """Driver={ODBC Driver 18 for SQL Server};
+                        Server=tcp:"""
+        + str(database_server_name)
+        + """.database.windows.net,1433;
+                        Database="""
+        + str(database_name)
+        + """;
+                        UID="""
+        + str(user_name)
+        + """;
+                        PWD="""
+        + str(user_password)
+        + """;
+                        Encrypt=yes;TrustServerCertificate=no;Connection Timeout=-1"""
+    )
     return pyodbc.connect(connection_string)
--- test/test.py
+++ test/test.py
@@ -1,7 +1,15 @@
 from io import StringIO
 import sys
+import os
 import unittest
+from dotenv import load_dotenv
 from src.main import Main
+
+load_dotenv()
+
+TICKETS = int(os.getenv("TICKETS"))
+T_MAX = int(os.getenv("T_MAX"))
+T_MIN = int(os.getenv("T_MIN"))
 
 
 class TestOxygenCs(unittest.TestCase):
@@ -9,28 +17,36 @@ class TestOxygenCs(unittest.TestCase):
         self.main = Main()
 
     def test_init_main(self):
-        self.assertEqual(self.main.TICKETS, 2)
-        self.assertEqual(self.main.T_MAX, 24)
-        self.assertEqual(self.main.T_MIN, 19)
+        self.assertEqual(self.main.TICKETS, TICKETS)
+        self.assertEqual(self.main.T_MAX, T_MAX)
+        self.assertEqual(self.main.T_MIN, T_MIN)
 
     def test_send_action_to_hvac(self):
         output = self.get_print(self.main.send_action_to_hvac, "TurnOnAc")
         self.assertEqual(output, "{'Response': 'Activating AC for 2 ticks'}")
 
-    def test_take_action_under_min(self):
-        output = self.get_print(self.main.take_action, 19)
+    def test_take_action_at_min(self):
+        output = self.get_print(self.main.take_action, T_MIN)
         self.assertEqual(output, "{'Response': 'Activating Heater for 2 ticks'}")
 
+    def test_take_action_under_min(self):
+        output = self.get_print(self.main.take_action, T_MIN - 1)
+        self.assertEqual(output, "{'Response': 'Activating Heater for 2 ticks'}")
+
+    def test_take_action_at_max(self):
+        output = self.get_print(self.main.take_action, T_MAX)
+        self.assertEqual(output, "{'Response': 'Activating AC for 2 ticks'}")
+
     def test_take_action_over_max(self):
-        output = self.get_print(self.main.take_action, 24)
+        output = self.get_print(self.main.take_action, T_MAX + 1)
         self.assertEqual(output, "{'Response': 'Activating AC for 2 ticks'}")
 
     def test_take_action_normal_temperature_min(self):
-        output = self.get_print(self.main.take_action, 20)
+        output = self.get_print(self.main.take_action, T_MIN + 1)
         self.assertEqual(output, "")
 
     def test_take_action_normal_temperature_max(self):
-        output = self.get_print(self.main.take_action, 23)
+        output = self.get_print(self.main.take_action, T_MAX - 1)
         self.assertEqual(output, "")
 
     def test_on_sensor_data_received(self):
